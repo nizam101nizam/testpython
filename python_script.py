2d2b0c438356de8e7140e1eae1c7f60236680e51
nizam101nizam <50822215+nizam101nizam@users.noreply.github.com>
2024-07-27 22:38:57 +0100

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -1,4 +1,5 @@
 import os
+import json
 data = os.getenv("input_data")
 print(data)
 print("Hello welocme to Python Git hub")
@@ -9,3 +10,18 @@ pwd= os.getcwd()
 files  = os.listdir()
 print(pwd)
 print(files)
+
+with open(sample1.json','r') as fil:
+     jdata1 = json.load(fil)
+
+print(jdata1)
+
+print('============')
+
+with open(home+'/sample1.json','r') as fil:
+     jdata = json.load(fil)
+
+print(jdata)
+
+
+
